7a08000800a6216b64502b763db342ef80aaf339
mahmoodi-majid <majid.mahmoodi.pg@gmail.com>
2023-05-16 14:28:15 +0330

    you can write in input

--- src/component/form/Form.js
+++ src/component/form/Form.js
@@ -1,10 +1,45 @@
 import React, { Component } from "react";
 import "./Form.css";
 export default class Form extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      allValid: false,
+      emailDate: "",
+      firstNameDate: "",
+      lastNameData: "",
+      submitted: false,
+    };
+  }
+  submitForm(e) {
+    e.preventDefault();
+    this.setState({
+      submitted: true,
+    });
+  }
+  changeFirstName(event) {
+    this.setState({
+      firstNameDate: event.target.value,
+    });
+  }
+  changeLastName(event) {
+    this.setState({
+      lastNameData: event.target.value,
+    });
+  }
+  changeEmailDate(event) {
+    this.setState({
+        emailDate: event.target.value,
+    });
+  }
   render() {
     return (
       <div className="form-container">
-        <form className="register-form" autoComplete="off">
+        <form
+          className="register-form"
+          autoComplete="off"
+          onSubmit={this.submitForm.bind(this)}
+        >
           {/* Uncomment the next line to show the success message */}
           {/* <div className="success-message">Success! Thank you for registering</div> */}
           <input
@@ -13,6 +48,9 @@ export default class Form extends Component {
             type="text"
             placeholder="First Name"
             name="firstName"
+            onChange={(event) => {
+              this.changeFirstName(event);
+            }}
           />
           {/* Uncomment the next line to show the error message */}
           {/* <span id="first-name-error">Please enter a first name</span> */}
@@ -22,6 +60,9 @@ export default class Form extends Component {
             type="text"
             placeholder="Last Name"
             name="lastName"
+            onChange={(event) => {
+              this.changeLastName(event);
+            }}
           />
           {/* Uncomment the next line to show the error message */}
           {/* <span id="last-name-error">Please enter a last name</span> */}
@@ -31,6 +72,9 @@ export default class Form extends Component {
             type="text"
             placeholder="Email"
             name="email"
+            onChange={(event) => {
+              this.changeEmailDate(event);
+            }}
           />
           {/* Uncomment the next line to show the error message */}
           {/* <span id="email-error">Please enter an email address</span> */}
