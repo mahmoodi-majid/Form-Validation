248b8d5f30ae2784051916f47079728423d3fa62
mahmoodi-majid <majid.mahmoodi.pg@gmail.com>
2023-08-20 00:12:30 +0330

    Changing the component class to the component function, connecting the data to the database and storing the information

--- src/component/form/Form.js
+++ src/component/form/Form.js
@@ -1,109 +1,79 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Form.css";
-export default class Form extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      allValid: false,
-      emailDate: "",
-      firstNameDate: "",
-      lastNameData: "",
-      submitted: false,
+
+export default function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const registerHandler = (event) => {
+    event.preventDefault();
+
+    let userInfo = {
+      firstName,
+      lastName,
+      email,  
     };
-  }
-  submitForm(e) {
-    e.preventDefault();
-    this.setState({
-      submitted: true,
-    });
-    if (
-      this.state.firstNameDate.length !== 0 &&
-      this.state.emailDate !== 0 &&
-      this.setState.lastNameData !== 0
-    ) {
-      this.setState({
-        allValid: true,
-      });
-    }
-    setTimeout(() => {
-      this.setState({ allValid: false });
-    }, 3000);
-  }
-  changeFirstName(event) {
-    this.setState({
-      firstNameDate: event.target.value,
-    });
-  }
-  changeLastName(event) {
-    this.setState({
-      lastNameData: event.target.value,
-    });
-  }
-  changeEmailDate(event) {
-    this.setState({
-      emailDate: event.target.value,
-    });
-  }
+    fetch('https://toplern-50a6b-default-rtdb.firebaseio.com/users.json' , {
+      method : 'POST',
+      body : JSON.stringify(userInfo)
+    }).then(response => {
+      console.log(response)
+    })
+  };
 
-  render() {
-    return (
-      <div className="form-container">
-        <form
-          className="register-form"
-          autoComplete="off"
-          onSubmit={this.submitForm.bind(this)}
-        >
-          {this.state.submitted && this.state.allValid && (
-            <div className="success-message">
-              Success! Thank you for registering
-            </div>
-          )}
-          <input
-            id="first-name"
-            className="form-field"
-            type="text"
-            placeholder="First Name"
-            name="firstName"
-            onChange={(event) => {
-              this.changeFirstName(event);
-            }}
-          />
-          {this.state.submitted && this.state.firstNameDate.length === 0 && (
-            <span id="first-name-error">Please enter a first name</span>
-          )}
+  return (
+    <div className="form-container">
+      <form
+        className="register-form"
+        autoComplete="off"
+        onSubmit={registerHandler}
+      >
+        {/* {this.state.submitted && this.state.allValid && (
+          <div className="success-message">
+            Success! Thank you for registering
+          </div>
+        )} */}
+        <input
+          id="first-name"
+          className="form-field"
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+          value={firstName}
+          onChange={(event) => {
+            setFirstName(event.target.value);
+          }}
+        />
 
-          <input
-            id="last-name"
-            className="form-field"
-            type="text"
-            placeholder="Last Name"
-            name="lastName"
-            onChange={(event) => {
-              this.changeLastName(event);
-            }}
-          />
-          {this.state.submitted && this.state.lastNameData.length === 0 && (
-            <span id="last-name-error">Please enter a last name</span>
-          )}
-          <input
-            id="email"
-            className="form-field"
-            type="text"
-            placeholder="Email"
-            name="email"
-            onChange={(event) => {
-              this.changeEmailDate(event);
-            }}
-          />
+        <input
+          id="last-name"
+          className="form-field"
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+          value={lastName}
+          onChange={(event) => {
+            setLastName(event.target.value);
+          }}
+        />
 
-          {this.state.submitted && this.state.emailDate.length === 0 && (
-            <span id="email-error">Please enter an email address</span>
-          )}
-          <button className="form-field" type="submit">
-            Register
-          </button>
-        </form>
-      </div>
-    );
-  }
+        <input
+          id="email"
+          className="form-field"
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
+
+        <button className="form-field" type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
